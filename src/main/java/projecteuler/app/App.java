064789789697c69af1a96a23b82248922938b454
Jeffery Hein <jefferyphein@gmail.com>
2022-05-29 16:44:14 -0400

    Update command-line to accept range-based problems to run.
    Can now specify "lower..upper" to the command-line interface to allow
    for running all problems from lower to upper, inclusive.
    
    Problem solutions are now printed along with their runtime.

--- src/main/java/projecteuler/app/App.java
+++ src/main/java/projecteuler/app/App.java
@@ -12,37 +12,61 @@ public class App {
     public static final String GREEN = "\u001B[32m";
     public static final String BLUE = "\u001B[34m";
 
+    public static void RunProblem(int problem_id) {
+        Euler problem = null;
+
+        // Create problem class from problem ID.
+        switch (problem_id) {
+        case 1: problem = new Euler001(); break;
+        case 2: problem = new Euler002(); break;
+        case 3: problem = new Euler003(); break;
+        case 4: problem = new Euler004(); break;
+        case 5: problem = new Euler005(); break;
+        case 6: problem = new Euler006(); break;
+        default:
+            System.out.println(
+                BLUE + String.format("Problem ID #%d: ", problem_id) +
+                RED + "does not exist" + RESET);
+            return;
+        }
+
+        // Run and time the selected problem.
+        long startTime = System.nanoTime();
+        String answer = problem.run();
+        long endTime = System.nanoTime();
+        double duration = (double)(endTime - startTime) / 1000000.0;
+
+        System.out.println(
+            BLUE + String.format("Problem ID #%d: ", problem_id) +
+            GREEN + answer + RESET + String.format(" (%.4f ms)", duration));
+    }
+
     public static void main( String[] args ) {
         // Provide usage message if no arguments are provided.
         if (args.length == 0) {
             System.err.println("Usage: app <problem-id> [<problem-id> [...]]");
+            System.err.println("Examples:");
+            System.err.println("  app 1 2 3     # run problems 1, 2, and 3");
+            System.err.println("  app 1..6      # run problems 1 through 6");
             return;
         }
 
         for (int i=0; i<args.length; i++) {
-            // Convert positional argument to problem ID.
-            int problem_id = Integer.parseInt(args[i]);
-            Euler problem = null;
+            if (args[i].contains("..")) {
+                // Run range-based problems.
+                int index = args[i].indexOf("..");
+                int lower = Integer.parseInt(args[i].substring(0, index));
+                int upper = Integer.parseInt(args[i].substring(index+2));
 
-            // Create problem class from problem ID.
-            switch (problem_id) {
-            case 1: problem = new Euler001(); break;
-            case 2: problem = new Euler002(); break;
-            case 3: problem = new Euler003(); break;
-            case 4: problem = new Euler004(); break;
-            case 5: problem = new Euler005(); break;
-            case 6: problem = new Euler006(); break;
-            default:
-                System.out.println(
-                    BLUE + String.format("Problem ID #%d: ", problem_id) +
-                    RED + "does not exist" + RESET);
-                continue;
+                for (int j=lower; j<=upper; j++) {
+                    RunProblem(j);
+                }
             }
-
-            // Run the selected problem and display the answer.
-            System.out.println(
-                BLUE + String.format("Problem ID #%d: ", problem_id) +
-                GREEN + problem.run() + RESET);
+            else {
+                // Convert argument to problem ID.
+                int problem_id = Integer.parseInt(args[i]);
+                RunProblem(problem_id);
+            }
         }
     }
 }
