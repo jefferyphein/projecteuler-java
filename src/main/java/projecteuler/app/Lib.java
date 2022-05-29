caa7d2a0697d03209e97539b7032ab6b2cb69548
Jeffery Hein <jefferyphein@gmail.com>
2022-05-29 19:19:26 -0400

    Add readLines function to Lib.java.

--- src/main/java/projecteuler/app/Lib.java
+++ src/main/java/projecteuler/app/Lib.java
@@ -1,8 +1,24 @@
 package projecteuler.app;
 
+import java.util.Vector;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.stream.Stream;
+
 public class Lib {
     public static boolean isPalindrome(long N) {
         String strN = Long.toString(N);
         return strN.equals(new StringBuilder(strN).reverse().toString());
+    }
+
+    public static Vector<String> readLines(String filename) throws IOException {
+        Vector<String> lines = new Vector<String>();
+
+        try (Stream<String> stream = Files.lines(Paths.get(filename))) {
+            stream.forEach(s -> lines.addElement(s));
+        }
+
+        return lines;
     }
 }
