b8d45b50691341ab3822b4f2b1e7ae568ab84e62
Jeffery Hein <jefferyphein@gmail.com>
2022-05-30 11:19:22 -0400

    Add unit tests for Lib.primesUpTo() function.

--- src/test/java/projecteuler/app/LibTest.java
+++ src/test/java/projecteuler/app/LibTest.java
@@ -26,4 +26,30 @@ public class LibTest {
         Vector<String> lines = Lib.readLines("./data/does_not_exist");
         assertEquals(lines, null);
     }
+
+    @Test
+    public void testPrimesUpToEmpty() {
+        int[] primes = Lib.primesUpTo(1);
+        assertEquals(primes.length, 0);
+    }
+
+    @Test
+    public void testPrimesUpToTwo() {
+        int[] primes = Lib.primesUpTo(2);
+        assertEquals(primes.length, 1);
+        assertEquals(primes[0], 2);
+    }
+
+    @Test
+    public void testPrimesUpTo100() {
+        int[] truePrimes = {
+            2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41,
+            43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97
+        };
+        int[] primes = Lib.primesUpTo(100);
+        assertEquals(primes.length, 25);
+        for (int i=0; i<primes.length; i++) {
+            assertEquals(primes[i], truePrimes[i]);
+        }
+    }
 }
