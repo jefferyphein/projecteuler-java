a7f35b8dc1ca2edbe82a1b5c35a5cb9306d49e09
Jeffery Hein <jefferyphein@gmail.com>
2022-05-30 10:07:01 -0400

    Add unit tests for Lib.readLines().

--- src/test/java/projecteuler/app/LibTest.java
+++ src/test/java/projecteuler/app/LibTest.java
@@ -3,6 +3,7 @@ package projecteuler.app;
 import static org.junit.Assert.*;
 
 import org.junit.Test;
+import java.util.Vector;
 
 /**
  * Unit tests for Project Euler library functions.
@@ -12,5 +13,17 @@ public class LibTest {
     public void testIsPalindrome() {
         assertTrue(Lib.isPalindrome(101));
         assertFalse(Lib.isPalindrome(400));
+    }
+
+    @Test
+    public void testReadLinesFileExists() {
+        Vector<String> lines = Lib.readLines("./data/008/input");
+        assertEquals(lines.size(), 20);
+    }
+
+    @Test
+    public void testReadLinesFileDoesNotExist() {
+        Vector<String> lines = Lib.readLines("./data/does_not_exist");
+        assertEquals(lines, null);
     }
 }
